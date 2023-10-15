17df25978b9c0ae71149200469cdb7c91aa28030
roalgl <ralbertogonzalezl@gmail.com>
2023-10-15 10:18:02 -0600

    Corriendo

--- src/main/java/com/omar/apiuniversidad/ApiUniversidadApplication.java
+++ src/main/java/com/omar/apiuniversidad/ApiUniversidadApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiUniversidadApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiUniversidadApplication.class, args);
+        SpringApplication.run(ApiUniversidadApplication.class, args).getApplicationName();
     }
 
 }
